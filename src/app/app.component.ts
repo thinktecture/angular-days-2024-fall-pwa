ef5958b2fdb37c01ab16ff3b13f9b451c4fe1c53
Christian Liebel <christian.liebel@thinktecture.com>
2024-10-22 14:51:55 +0200

    Lab #6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,6 +56,7 @@ export class AppComponent implements AfterViewInit {
 
   onColorChange(color: HTMLInputElement) {
     // LAB #6
+    this.context.fillStyle = color.value;
   }
 
   async open() {
